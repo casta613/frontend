d219a18242a43c804bcf5a54bc6a18c196d9844d
Jeffry999 <jeffrymaquiz02@gmail.com>
2023-09-06 22:06:19 -0600

    commit

---
+++ src/app/iniciar-sesion/iniciar-sesion.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IniciarSesionRoutingModule } from './iniciar-sesion-routing.module';
+import { LoginComponent } from './componentes/Login/login.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    IniciarSesionRoutingModule,
+    SharedModule
+  ]
+})
+export class IniciarSesionModule { }
